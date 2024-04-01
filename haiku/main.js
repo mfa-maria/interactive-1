2b21e78eb2eff6a0d25f08c90ae10f2bc85ce398
mfa-maria <158335346+mfa-maria@users.noreply.github.com>
2024-04-01 12:10:40 -0700

    HAIKU publish

---
+++ haiku/main.js
@@ -0,0 +1,23 @@
+// Get viewport width and height
+let viewportW = window.innerWidth;
+let viewportH = window.innerHeight;
+
+// try changing "mousemove" to "click"
+document.addEventListener("mousemove", update, true);
+document.addEventListener("scroll", update, true);
+
+function update(e) {
+  let mouseY = e.clientY;
+  let scrollY = window.scrollY;
+  let text = "";
+  
+  if (mouseY < viewportH/2) {
+    text = "green with spring"
+  }
+  else {
+    text = "yellow with fall";
+  }
+  
+  document.getElementById("t").innerText = text;
+ document.getElementById("t").style.color = "limegreen" ;
+}
---
+++ js-demo/main.js
@@ -0,0 +1,26 @@
+let apples = 4;
+let oranges = 5;
+let message = "";
+
+if (apples >= oranges) {
+	message = "There are more apples than oranges.";
+}
+else{
+	message = "There are more oranges than apples.";
+}
+
+document.getElementById("text").innerText = message; 
+
+
+
+
+//let age = 32;           //store value into a valuable
+//age = age * 30 / 4;  	// process it 
+//console.log(age);		// out to console 
+
+///let name ="Roon";
+//let welcome= "Hi there, ";
+//let message = welcome + name; 
+
+//output to main element
+//document.getElementById("text").innerText = message; 
